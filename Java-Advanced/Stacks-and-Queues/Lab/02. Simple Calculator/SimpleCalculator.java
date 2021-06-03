0d327138da800448985aab8719ca7ea68435d96c
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-06-03 20:26:26 +0300

    Update SimpleCalculator.java

--- Java-Advanced/Stacks-and-Queues/Lab/02. Simple Calculator/SimpleCalculator.java
+++ Java-Advanced/Stacks-and-Queues/Lab/02. Simple Calculator/SimpleCalculator.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class SimpleCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
