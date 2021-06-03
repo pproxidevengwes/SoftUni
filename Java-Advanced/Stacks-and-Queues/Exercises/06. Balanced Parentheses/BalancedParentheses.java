f09ee8f342563fc723caaa29fa69aabd055ee13d
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-06-03 20:45:24 +0300

    Update BalancedParentheses.java

--- Java-Advanced/Stacks-and-Queues/Exercises/06. Balanced Parentheses/BalancedParentheses.java
+++ Java-Advanced/Stacks-and-Queues/Exercises/06. Balanced Parentheses/BalancedParentheses.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.*;
 
-public class Main {
+public class BalancedParentheses {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
