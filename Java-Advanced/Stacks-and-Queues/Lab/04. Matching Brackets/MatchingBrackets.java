bf21e4bdef28e5eacc36d005aec664d34f399a49
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-06-03 20:27:03 +0300

    Create MatchingBrackets.java

---
+++ Java-Advanced/Stacks-and-Queues/Lab/04. Matching Brackets/MatchingBrackets.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.*;
+
+public class MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int decimal = Integer.valueOf(scanner.nextLine());
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        if (decimal == 0) {
+            System.out.println(0);
+        }
+        while (decimal != 0) {
+            stack.push(decimal % 2);
+            decimal /= 2;
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+    }
+}
