80e92218f60dff6ad011b345199667950619d3a2
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-06-05 16:58:11 +0300

    Update and rename ALLCAPITALS.java to AllCapitals.java

--- Java-Advanced/Streams-Files-And-Directories/Exercises/3. ALL CAPITALS!/AllCapitals.java
+++ Java-Advanced/Streams-Files-And-Directories/Exercises/3. ALL CAPITALS!/AllCapitals.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 
-public class ALLCAPITALS {
+public class AllCapitals {
     public static int countRow = 1;
     public static void main(String[] args) throws IOException {
         String path ="C:\\Users\\Admin\\Downloads\\04. Java-Advanced-Files-and-Streams-Exercises-Resources/input.txt";
