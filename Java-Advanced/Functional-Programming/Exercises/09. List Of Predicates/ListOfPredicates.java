c023457da03c8cc73035404b823d67344ae6e144
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-06-07 15:03:23 +0300

    Update and rename Java-Advanced/Functional-Programming/Exercises/9. List Of Predicates/ListOfPredicates.java to Java-Advanced/Functional-Programming/Exercises/09. List Of Predicates/ListOfPredicates.java

--- Java-Advanced/Functional-Programming/Exercises/09. List Of Predicates/ListOfPredicates.java
+++ Java-Advanced/Functional-Programming/Exercises/09. List Of Predicates/ListOfPredicates.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.util.function.Predicate;
 
 
-public class Main {
+public class ListOfPredicates {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
