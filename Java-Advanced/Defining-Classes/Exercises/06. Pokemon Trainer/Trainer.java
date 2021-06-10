96d80019bc8352b21084d449a47873d3c16b33c4
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-06-10 16:52:36 +0300

    Update Trainer.java

--- Java-Advanced/Defining-Classes/Exercises/06. Pokemon Trainer/Trainer.java
+++ Java-Advanced/Defining-Classes/Exercises/06. Pokemon Trainer/Trainer.java
@@ -1,14 +1,53 @@
-public class Pokemon {
-   private String pokemonName;
-   private String pokemonType;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Pokemon(String pokemonName, String pokemonType) {
-        this.pokemonName = pokemonName;
-        this.pokemonType = pokemonType;
+public class Trainer {
+    private String trainerName;
+    private int badges;
+    private List<Pokemon> pokemons;
+
+    public Trainer(String trainerName) {
+        this.trainerName = trainerName;
+        this.badges = 0;
+        this.pokemons = new ArrayList<>();
+    }
+
+    public boolean checkForPokemons(String element) {
+        for (Pokemon pokemon : pokemons) {
+            if (pokemon.getElement().equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void reducePokemonHealth() {
+        for (int i = 0; i < pokemons.size(); i++) {
+            pokemons.get(i).setHealth(pokemons.get(i).getHealth() - 10);
+            if (pokemons.get(i).getHealth() <= 0) {
+                pokemons.remove(pokemons.get(i));
+                i--;
+            }
+        }
+    }
+
+
+    public void addPokemon(Pokemon pokemon) {
+        pokemons.add(pokemon);
+    }
+
+    public void increaseBadges() {
+        badges++;
+    }
+
+    public int getBadges() {
+        return badges;
     }
 
     @Override
     public String toString() {
-        return pokemonName + " " + pokemonType;
+        return this.trainerName + " " +
+                this.badges +
+                " " + this.pokemons.size();
     }
 }
