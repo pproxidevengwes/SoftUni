6687cfb6547b02529a4b37303a5abac826930658
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-06-14 22:15:53 +0300

    Update Main.java

--- Java-Advanced/Generics/Lab/01. Jar of T/Main.java
+++ Java-Advanced/Generics/Lab/01. Jar of T/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        
         Jar<Integer> integerJar = new Jar<>();
         
         integerJar.add(13);
