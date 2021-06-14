d6e2735c2591d038c205b884334008e93695d009
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-06-14 22:15:23 +0300

    Update Main.java

--- Java-Advanced/Generics/Lab/02. Generic Array Creator/Main.java
+++ Java-Advanced/Generics/Lab/02. Generic Array Creator/Main.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         String[] javas = ArrayCreator.<String>create(String.class, 13, "Java");
     }
 }
