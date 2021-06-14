270e67fa9f6b05da55536df1eb39ab178ce8dd9c
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-06-14 22:24:22 +0300

    Update ArrayCreator.java

--- Java-Advanced/Generics/Lab/02. Generic Array Creator/ArrayCreator.java
+++ Java-Advanced/Generics/Lab/02. Generic Array Creator/ArrayCreator.java
@@ -1,17 +1,20 @@
+
 import java.lang.reflect.Array;
 import java.util.stream.IntStream;
 
-public class ArrayCreator<T> {
+public class ArrayCreator {
 
-    public static <T> T[] create(int length, T value) {
+
+
+    @SuppressWarnings("unchecked")
+    public static <T> T[] create(int length, T value){
         return create(value.getClass(), length, value);
     }
 
-    public static <T> T[] create(Class<?> clazz, int length, T value) {
-        T[] arr = (T[]) Array.newInstance(clazz, length);
+    public static <T> T[] create(Class<?> clazz, int length, T value){
+        T[] arr = (T[])Array.newInstance(clazz, length);
         IntStream.range(0, length)
                 .forEach(e -> arr[e] = value);
         return arr;
     }
-
 }
