bcfe4a1ef0e851a7f49b2a2c3c67a1eb4d5c219f
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-06-17 19:49:21 +0300

    Update Main.java

--- Java-Advanced/Iterators and Comparators/Lab/Library/Main.java
+++ Java-Advanced/Iterators and Comparators/Lab/Library/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         Book book1 = new Book("Animal Farm", 2003, "George Orwell");
         Book book3 = new Book("The Documents in the Case", 2002);
         Book book2 = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
