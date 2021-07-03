e7ef3ae7af4003016d2ff0426331c911b559855c
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-07-03 15:58:49 +0300

    Update Main.java

--- Java-OOP/Working-with-Abstraction/Lab/Point in Rectangle/Main.java
+++ Java-OOP/Working-with-Abstraction/Lab/Point in Rectangle/Main.java
@@ -2,7 +2,7 @@ import input.Reader;
 
 public class Main {
     public static void main(String[] args) {
-        int[] coordinates = Reader.readArray("\\s+");
+        int[] coordinates = Reader.readIntArray("\\s+");
 
         Point A = new Point(coordinates[0], coordinates[1]);
         Point B = new Point(coordinates[2], coordinates[3]);
@@ -11,11 +11,11 @@ public class Main {
 
         int n = Reader.readArray("\\s+")[0];
         while (n-- > 0) {
-            int[] singlePoint = Reader.readArray("\\s+");
+            int[] singlePoint = Reader.readIntArray("\\s+");
             Point p = new Point(singlePoint[0], singlePoint[1]);
 
             boolean isWithin = rect.contains(p);
-            System.out.println(isWithin);
+            printLine(isWithin);
         }
 
     }
