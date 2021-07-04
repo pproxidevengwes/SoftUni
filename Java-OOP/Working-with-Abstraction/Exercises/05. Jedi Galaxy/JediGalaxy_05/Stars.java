06687befc586559b2f15ef2e9e7bdcda889dd297
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-07-04 16:14:53 +0300

    Create Stars.java

---
+++ Java-OOP/Working-with-Abstraction/Exercises/05. Jedi Galaxy/JediGalaxy_05/Stars.java
@@ -0,0 +1,13 @@
+package JediGalaxy_05;
+
+public class Star {
+    private int value;
+
+    public Star(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
