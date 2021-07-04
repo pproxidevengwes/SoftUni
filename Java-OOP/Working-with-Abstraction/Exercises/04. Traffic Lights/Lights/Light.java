e0c145b5375c8c2fe1f0de4619f1b38b97fd5a63
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-07-04 15:43:09 +0300

    Create Light.java

---
+++ Java-OOP/Working-with-Abstraction/Exercises/04. Traffic Lights/Lights/Light.java
@@ -0,0 +1,29 @@
+package Lights;
+
+public class Light {
+    private Color color;
+
+    public Light(Color color) {
+        this.color = color;
+    }
+
+    public void changeColor(){
+        switch (this.color){
+
+            case RED:
+                this.color=Color.GREEN;
+                break;
+            case GREEN:
+                this.color=Color.YELLOW;
+                break;
+            case YELLOW:
+                this.color=Color.RED;
+
+                break;
+        }
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
