0ecf1eb11d520769f586f232ed748a57cedb33bc
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-07-05 15:49:31 +0300

    Create Person.java

---
+++ Java-OOP/Encapsulation/Lab/02. SalaryIncrease/Person.java
@@ -0,0 +1,45 @@
+public class Person {
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double bonus) {
+        if (this.getAge() < 30) {
+            bonus /= 2;
+        }
+        this.setSalary(this.salary *= (1.0 + bonus / 100));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %s leva", this.getFirstName(), getLastName(), getSalary());
+    }
+}
