4f7f297c7b6fc077e70964f718639d1cdbaeec3e
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-07-05 17:47:45 +0300

    Create Team.java

---
+++ Java-OOP/Encapsulation/Lab/04. First And Reserve Team/Team.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private final List<Person> firstTeam;
+    private final List<Person> reserveTeam;
+
+    public Team(String name) {
+        setName(name);
+        this.firstTeam = new ArrayList<>();
+        this.reserveTeam = new ArrayList<>();
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Person> getFirstTeam() {
+        return Collections.unmodifiableList(this.firstTeam);
+    }
+
+    public List<Person> getReserveTeam() {
+        return Collections.unmodifiableList(this.reserveTeam);
+
+    }
+
+    public void addPlayer(Person person) {
+        if (person.getAge() < 40) {
+            firstTeam.add(person);
+        } else {
+            reserveTeam.add(person);
+        }
+    }
+}
