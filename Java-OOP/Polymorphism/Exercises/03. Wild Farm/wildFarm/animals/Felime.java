ba63f6b3bcded65dcc48414eba54a94996cb3adf
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-07-17 15:55:12 +0300

    Create Felime.java

---
+++ Java-OOP/Polymorphism/Exercises/03. Wild Farm/wildFarm/animals/Felime.java
@@ -0,0 +1,7 @@
+package wildFarm.animals;
+
+public abstract class Felime extends Mammal {
+    protected Felime(String name, String type, double weight, String region) {
+        super(name, type, weight, region);
+    }
+}
