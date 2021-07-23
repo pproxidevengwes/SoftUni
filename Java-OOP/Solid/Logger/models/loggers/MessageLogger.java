9b29784b5fe687011f9ad5c56b52905a5b14552c
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-07-23 14:41:50 +0300

    Create MessageLogger.java

---
+++ Java-OOP/Solid/Logger/models/loggers/MessageLogger.java
@@ -0,0 +1,68 @@
+package models.loggers;
+
+import enums.ReportLevel;
+import interfaces.Appender;
+import interfaces.Layout;
+import interfaces.Logger;
+
+public class MessageLogger implements Logger {
+    private Appender[] appenders;
+
+    public MessageLogger(Appender... appenders) {
+        this.appenders = appenders;
+    }
+
+    @Override
+    public void logInfo(String dateTime, String message) {
+        this.logMessage(dateTime, ReportLevel.INFO, message);
+
+    }
+
+    @Override
+    public void logWarning(String dateTime, String message) {
+        this.logMessage(dateTime, ReportLevel.WARNING, message);
+
+    }
+
+    @Override
+    public void logError(String dateTime, String message) {
+        this.logMessage(dateTime, ReportLevel.ERROR, message);
+
+    }
+
+    @Override
+    public void logCritical(String dateTime, String message) {
+        this.logMessage(dateTime, ReportLevel.CRITICAL, message);
+
+    }
+
+    @Override
+    public void logFatal(String dateTime, String message) {
+        this.logMessage(dateTime, ReportLevel.FATAL, message);
+
+    }
+
+    private void logMessage(String dateTime, ReportLevel reportLevel, String message) {
+        for (Appender appender : this.appenders) {
+            appender.appendMessage(dateTime, reportLevel, message);
+        }
+    }
+
+    @Override
+    public String getLogInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Logger info");
+        for (Appender appender : this.appenders) {
+            sb.append(System.lineSeparator())
+                    .append(appender);
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public void logInfo(String logInfo) {
+        StringBuilder sb = new StringBuilder();
+
+    }
+}
