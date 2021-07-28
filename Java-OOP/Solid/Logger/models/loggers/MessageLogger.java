ffde2dac19b3681ccb5c6c1850708aaf5b039d1a
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-07-28 17:24:16 +0300

    Update MessageLogger.java

--- Java-OOP/Solid/Logger/models/loggers/MessageLogger.java
+++ Java-OOP/Solid/Logger/models/loggers/MessageLogger.java
@@ -2,45 +2,40 @@ package models.loggers;
 
 import enums.ReportLevel;
 import interfaces.Appender;
-import interfaces.Layout;
 import interfaces.Logger;
 
 public class MessageLogger implements Logger {
     private Appender[] appenders;
 
-    public MessageLogger(Appender... appenders) {
-        this.appenders = appenders;
+    public MessageLogger(Appender... appender) {
+        this.appenders = appender;
     }
 
     @Override
     public void logInfo(String dateTime, String message) {
         this.logMessage(dateTime, ReportLevel.INFO, message);
-
     }
 
     @Override
     public void logWarning(String dateTime, String message) {
         this.logMessage(dateTime, ReportLevel.WARNING, message);
-
     }
 
     @Override
     public void logError(String dateTime, String message) {
         this.logMessage(dateTime, ReportLevel.ERROR, message);
-
     }
 
     @Override
     public void logCritical(String dateTime, String message) {
         this.logMessage(dateTime, ReportLevel.CRITICAL, message);
-
     }
 
     @Override
     public void logFatal(String dateTime, String message) {
         this.logMessage(dateTime, ReportLevel.FATAL, message);
-
     }
+
 
     private void logMessage(String dateTime, ReportLevel reportLevel, String message) {
         for (Appender appender : this.appenders) {
@@ -53,16 +48,9 @@ public class MessageLogger implements Logger {
         StringBuilder sb = new StringBuilder();
         sb.append("Logger info");
         for (Appender appender : this.appenders) {
-            sb.append(System.lineSeparator())
-                    .append(appender);
+            sb.append(System.lineSeparator()).append(appender);
         }
 
         return sb.toString();
-    }
-
-    @Override
-    public void logInfo(String logInfo) {
-        StringBuilder sb = new StringBuilder();
-
     }
 }
