e7ce082c59ed1b597e31f2c53a6f5ff5e7f521bd
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-08-06 16:13:22 +0300

    Create Validator.java

---
+++ Java-OOP/Exception-Handling/Lab/customException/Validator.java
@@ -0,0 +1,19 @@
+package customException;
+
+public class Validator {
+    public Validator() {
+    }
+
+    protected static void ValidateName(String name) {
+        if (!name.matches("^[A-Za-z]+$")) {
+            throw new InvalidPersonNameException("Invalid name: " + name + ". The name cannot contain any special character or numeric value." +
+                    "Please, enter valid name.");
+        }
+    }
+
+    protected static void ValidateEmail(String email) {
+        if (!email.contains("@")) {
+            throw new InvalidPersonNameException("Invalid email: " + email + ". Please, enter valid email address.");
+        }
+    }
+}
