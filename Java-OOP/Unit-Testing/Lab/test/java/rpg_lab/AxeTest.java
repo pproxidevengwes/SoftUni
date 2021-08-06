69d9d532da7f3c9a25382a319655f6bf016097aa
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-08-06 17:04:45 +0300

    Update AxeTest.java

--- Java-OOP/Unit-Testing/Lab/test/java/rpg_lab/AxeTest.java
+++ Java-OOP/Unit-Testing/Lab/test/java/rpg_lab/AxeTest.java
@@ -1,39 +1,46 @@
-package test.java.rpg_lab;
+package rpg_lab;
 
-import main.java.rpg_lab.Axe;
-import main.java.rpg_lab.Dummy;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class AxeTest {
+import static org.junit.Assert.assertEquals;
 
-    private static final int AXE_ATTACK = 10;
-    private static final int AXE_DURABILITY = 2;
-    private static final int DUMMY_HEALTH = 11;
-    private static final int DUMMY_EXPERIENCE = 13;
-    private static final int EXPECTED_DURABILITY = AXE_DURABILITY - 1;
+public class AxeTest {
+    private final static int ATTACK = 10;
+    private final static int DURABILITY = 10;
 
     private Axe axe;
     private Dummy dummy;
 
     @Before
     public void setUp() {
-        this.axe = new Axe(AXE_ATTACK, AXE_DURABILITY);
-        this.dummy = new Dummy(DUMMY_HEALTH, DUMMY_EXPERIENCE);
+        this.axe = createAxe(ATTACK, DURABILITY);
+        this.dummy = createDummy();
+    }
+
+    private Axe createAxe(int attack, int durability) {
+        return new Axe(attack, durability);
+    }
+
+    private Dummy createDummy() {
+        return new Dummy(10, 10);
     }
 
     @Test
-    public void weaponAttacksLosesDurability() {
-        this.axe.attack(dummy);
+    public void testAxeConstructorShouldSetCorrectAttackAndDurability() {
+        assertEquals(ATTACK, this.axe.getAttackPoints());
+        assertEquals(DURABILITY, this.axe.getDurabilityPoints());
+    }
 
-        Assert.assertEquals(EXPECTED_DURABILITY, axe.getDurabilityPoints());
+    @Test
+    public void testAxeShouldLoosesSingleDurabilityPointWhenAttackingDummy() {
+        this.axe.attack(this.dummy);
+        assertEquals(DURABILITY - 1, this.axe.getDurabilityPoints());
     }
 
     @Test(expected = IllegalStateException.class)
-    public void brokenWeaponCantAttack() {
-        this.axe.attack(dummy);
-        this.axe.attack(dummy);
-        this.axe.attack(dummy);
+    public void testAxeAttackShouldFailIfAxeHasZeroDurability() {
+        Axe axe = createAxe(ATTACK, 0);
+        axe.attack(this.dummy);
     }
 }
