665b738b6a8ad82f6cf9b547a50fa11e483fea33
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-08-06 15:59:22 +0300

    Update Fixing.java

--- Java-OOP/Exception-Handling/Lab/Fixing.java
+++ Java-OOP/Exception-Handling/Lab/Fixing.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CustomException {
+public class Fixing {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
