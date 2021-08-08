bf323b9b49eba00c0f115afb7ae1a2a2db3fcf7e
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-08-08 16:53:06 +0300

    Update PlayerTests.java

--- Java-OOP/Past-Exams/12.04.20/CounterStriker/HalfLife/src/test/java/halfLife/PlayerTests.java
+++ Java-OOP/Past-Exams/12.04.20/CounterStriker/HalfLife/src/test/java/halfLife/PlayerTests.java
@@ -1,77 +1,102 @@
-package halfLife;
-
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PlayerTests {
-
     private Player player;
+    private List<Player> playerList;
+    private Gun gun;
+    private List<Gun> guns;
+
+    @Before
+    public void setUp() {
+        playerList = new ArrayList<>();
+        player = new Player("Pesho", 120);
+        playerList.add(player);
+        guns = new ArrayList<>();
+        gun = new Gun("Pistol", 30);
+        guns.add(gun);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testPlayerConstructorShouldThrowExWhenUsernameIsNull() {
+        player = new Player(null, 10);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testPlayerConstructorShouldThrowExWhenUsernameTrimmedIsNUll() {
+        player = new Player("   ", 10);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPlayerConstructorShouldThrowExWhenHealthIsLessThanZero() {
+        player = new Player("Gosho", -1);
+    }
 
     @Test
-    public void testConstructor() {
-        Player newPlayer = new Player("Teest", 123);
-
-        assertEquals("Teest", newPlayer.getUsername());
-        assertEquals(123, newPlayer.getHealth());
+    public void testConstructorShouldCreateCorrectPlayer() {
+        Player secondPlayer = new Player("Gosho", 50);
+        playerList.add(secondPlayer);
+        assertEquals(2, playerList.size());
+        assertEquals("Gosho", playerList.get(1).getUsername());
+        assertEquals(50, playerList.get(1).getHealth());
     }
 
-    @Test (expected = NullPointerException.class)
-    public void testNullUserNameException () {
-        player = new Player("",100);
+    @Test(expected = UnsupportedOperationException.class)
+    public void testUnmodifiableCollectionShouldThrowExWhenRemove() {
+        player.getGuns().remove(0);
     }
 
-    @Test (expected = NullPointerException.class)
-    public void testEmptySpaceUserNameException () {
-        player = new Player("   ", 99);
+    @Test(expected = IllegalStateException.class)
+    public void testTakeDamageShouldThrowExWhenHealthIsLessThanZero() {
+        player = new Player("Sasho", 0);
+        player.takeDamage(120);
     }
 
-    @Test (expected = IllegalArgumentException.class)
-    public void testHealthLessThanZero () {
-        player = new Player("XXX", - 10);
+    @Test
+    public void testTakeDamageShouldSetHealthToZero() {
+        player.takeDamage(130);
+        assertEquals(0, player.getHealth());
     }
 
-    @Test (expected = NullPointerException.class)
-    public void testAddNullGun () {
-        Gun gun = null;
-        player = new Player("Tommy", 100);
+    @Test
+    public void testTakeDamageShouldDecreaseHealth() {
+        player.takeDamage(30);
+        assertEquals(90, player.getHealth());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAddNullGunShouldThrowEx() {
+        player.addGun(null);
+    }
+
+    @Test
+    public void testAddGunShouldAddCorrectGun() {
+        Gun secondGun = new Gun("Pistol", 5);
+        guns.add(secondGun);
+        assertEquals(2, guns.size());
+        assertEquals("Pistol", guns.get(1).getName());
+        assertEquals(5, guns.get(1).getBullets());
+    }
+
+    @Test
+    public void testRemoveExistingGun() {
         player.addGun(gun);
+        boolean removeGun = player.removeGun(gun);
+        assertTrue(removeGun);
     }
 
     @Test
-    public void testAddGun () {
-        Gun gun = new Gun("DEagle", 21);
-        player = new Player("Tommy", 100);
+    public void testGetGunShouldReturnFirstGunWithThatName() {
         player.addGun(gun);
-
-        assertEquals(gun, player.getGun("DEagle"));
-    }
-
-    @Test
-    public void testRemoveGun () {
-        Gun gun = new Gun("DEagle", 21);
-        player = new Player("Tommy", 100);
-        player.addGun(gun);
-        assertTrue(player.removeGun(gun));
-    }
-
-    @Test
-    public void testGetGun () {
-        Gun gun = new Gun("DEagle", 21);
-        player = new Player("Tommy", 100);
-        player.addGun(gun);
-
-        Gun expected = gun;
-        Gun actual = player.getGun("DEagle");
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testDamageMore() {
-        player = new Player("Tommy", 100);
-        player.takeDamage(50);
-        assertEquals(50, player.getHealth());
+        player.addGun(new Gun("Gun", 55));
+        player.addGun(new Gun("Pistol", 22));
+        assertEquals(3, player.getGuns().size());
+        assertEquals(30, player.getGun("Pistol").getBullets());
     }
 }
