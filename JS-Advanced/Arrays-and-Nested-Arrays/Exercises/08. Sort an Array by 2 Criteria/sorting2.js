895f4407929c28b9309203195bbe46ed753a5d3b
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2021-10-08 13:15:41 +0300

    Create sorting2.js

---
+++ JS-Advanced/Arrays-and-Nested-Arrays/Exercises/08. Sort an Array by 2 Criteria/sorting2.js
@@ -0,0 +1,32 @@
+function solve(arr) {
+    let result = arr.sort((a, b) => {
+        if (a.length > b.length) {
+            return 1;
+        } else if (a.length == b.length) {
+            return a.localeCompare(b);
+        } else {
+            return -1;
+        }
+    })
+    console.log(result.join('\n'));
+}
+
+solve(['alpha',
+    'beta',
+    'gamma']
+);
+console.log('---');
+
+solve(['Isacc',
+    'Theodor',
+    'Jack',
+    'Harrison',
+    'George']
+);
+console.log('---');
+
+solve(['test',
+    'Deny',
+    'omen',
+    'Default']
+);
