50608b24f052d189da04824bba20b508238c54f7
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-01-14 22:00:20 +0200

    Update lab.sql

--- MySQL/Basic-CRUD/Lab/lab.sql
+++ MySQL/Basic-CRUD/Lab/lab.sql
@@ -8,4 +8,26 @@ FROM `employees`
 WHERE salary>1000
 ORDER BY id ASC;
 
+#03. Update Salary and Select
+UPDATE employees 
+SET salary = salary + 100
+WHERE job_title = 'Manager';
 
+SELECT salary FROM employees;
+
+#04. Top Paid Employee
+CREATE VIEW `v_top_paid` AS
+SELECT * FROM employees
+ORDER BY salary DESC
+LIMIT 1;
+SELECT * FROM `v_top_paid`;
+
+#05. Select Employees by Multiple Filters
+SELECT * FROM employees
+WHERE department_id = 4 AND salary >= 1000
+ORDER BY id;
+
+#06. Delete from Table
+DELETE FROM employees
+WHERE department_id IN (1,2);
+SELECT * FROM employees;
