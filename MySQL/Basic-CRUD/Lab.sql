ec9ab1818b65563a41491211b248bdbca733b445
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-01-14 21:32:56 +0200

    Create Lab.sql

---
+++ MySQL/Basic-CRUD/Lab.sql
@@ -0,0 +1,11 @@
+#01. Select Employee Information
+SELECT `id`, `first_name`, `last_name`, `job_title` FROM `employees`
+ORDER BY `id`;
+
+#02. Select Employees with Filter
+SELECT `id`, concat_ws(' ', `first_name`, `last_name`) AS 'full_name' , `job_title`, `salary`
+FROM `employees`
+WHERE salary>1000
+ORDER BY id ASC;
+
+
