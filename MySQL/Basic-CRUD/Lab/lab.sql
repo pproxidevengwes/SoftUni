b4100b331441e7f0da80f43396b34646ba4cc404
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-01-17 22:01:20 +0200

    Update lab.sql

--- MySQL/Basic-CRUD/Lab/lab.sql
+++ MySQL/Basic-CRUD/Lab/lab.sql
@@ -1,6 +1,6 @@
 #01. Select Employee Information
 SELECT `id`, `first_name`, `last_name`, `job_title` FROM `employees`
-ORDER BY `id`;
+ORDER BY id;
 
 #02. Select Employees with Filter
 SELECT `id`, concat_ws(' ', `first_name`, `last_name`) AS 'full_name' , `job_title`, `salary`
