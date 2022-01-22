437b25f875d7ab608249a3f2d5ffa7acb719b75b
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-01-22 23:15:07 +0200

    Update lab.sql

--- MySQL/Basic-CRUD/Lab/lab.sql
+++ MySQL/Basic-CRUD/Lab/lab.sql
@@ -1,11 +1,11 @@
 #01. Select Employee Information
-SELECT `id`, `first_name`, `last_name`, `job_title` FROM `employees`
+SELECT id, first_name, last_name, job_title FROM employees
 ORDER BY id;
 
 #02. Select Employees with Filter
-SELECT `id`, concat_ws(' ', `first_name`, `last_name`) AS 'full_name' , `job_title`, `salary`
-FROM `employees`
-WHERE salary>1000
+SELECT id, concat_ws(' ', first_name, last_name) AS 'full_name' , job_title, salary
+FROM employees
+WHERE salary > 1000
 ORDER BY id ASC;
 
 #03. Update Salary and Select
