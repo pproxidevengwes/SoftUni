d0cb6e76671f98d375d947d1ab0b2aebaf7bb82a
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-01-22 23:16:11 +0200

    Update exe_pt1.sql

--- MySQL/Basic-CRUD/Exercises/soft_uni/exe_pt1.sql
+++ MySQL/Basic-CRUD/Exercises/soft_uni/exe_pt1.sql
@@ -101,5 +101,5 @@ LIMIT 7;
 #20. Increase Salaries
 UPDATE employees
 SET salary = salary + (salary * 0.12)
-WHERE `department_id` IN (1, 2, 4, 11);
+WHERE department_id IN (1, 2, 4, 11);
 SELECT salary FROM employees;
