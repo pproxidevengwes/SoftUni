93fe1d6d0691c27bab0f91044751be5e0ec27ac1
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-01-22 23:11:37 +0200

    Update exe_pt2.sql

--- MySQL/Built-in-Functions/Exercises/geography/exe_pt2.sql
+++ MySQL/Built-in-Functions/Exercises/geography/exe_pt2.sql
@@ -8,7 +8,8 @@ ORDER BY iso_code;
 
 #11. Mix of Peak and River Names
 SELECT p.peak_name, r.river_name, 
-lower(concat(left(p.peak_name, char_length(p.peak_name) - 1), r.river_name)) AS 'mix'
+lower(concat(left(p.peak_name, char_length(p.peak_name) - 1),
+r.river_name)) AS 'mix'
 FROM peaks AS p, rivers AS r
 WHERE lower(right(p.peak_name, 1)) = lower(left(r.river_name, 1))
 ORDER BY mix;
