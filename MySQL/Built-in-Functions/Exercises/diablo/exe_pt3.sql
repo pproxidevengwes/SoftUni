867308895f917c28f2d464057007583c97bf8299
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-01-22 23:13:21 +0200

    Update exe_pt3.sql

--- MySQL/Built-in-Functions/Exercises/diablo/exe_pt3.sql
+++ MySQL/Built-in-Functions/Exercises/diablo/exe_pt3.sql
@@ -19,15 +19,15 @@ ORDER BY user_name;
 
 #15. Show All Games with Duration
 SELECT name,
- (CASE 
-  WHEN HOUR(g.start) BETWEEN 0 AND 11 THEN 'Morning'
-  WHEN HOUR(g.start) BETWEEN 12 AND 17 THEN 'Afternoon'
-  ELSE 'Evening'
- END) AS 'Part of the Day',
- (CASE
-  WHEN g.duration <= 3 THEN 'Extra Short'
-  WHEN g.duration BETWEEN 4 AND 6 THEN 'Short'
-  WHEN g.duration BETWEEN 7 AND 10 THEN 'Long'
-  ELSE 'Extra Long'
- END) AS 'Duration'
+(CASE 
+ WHEN HOUR(g.start) BETWEEN 0 AND 11 THEN 'Morning'
+ WHEN HOUR(g.start) BETWEEN 12 AND 17 THEN 'Afternoon'
+ ELSE 'Evening'
+END) AS 'Part of the Day',
+(CASE
+ WHEN g.duration <= 3 THEN 'Extra Short'
+ WHEN g.duration BETWEEN 4 AND 6 THEN 'Short'
+ WHEN g.duration BETWEEN 7 AND 10 THEN 'Long'
+ ELSE 'Extra Long'
+END) AS 'Duration'
 FROM games AS g;
