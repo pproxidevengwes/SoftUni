514353bb0454e80a751756e0766d377105498126
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-01-22 23:17:00 +0200

    Update exe_pt2.sql

--- MySQL/Basic-CRUD/Exercises/geography/exe_pt2.sql
+++ MySQL/Basic-CRUD/Exercises/geography/exe_pt2.sql
@@ -15,6 +15,6 @@ LIMIT 30;
 
 #23. Countries and Currency (Euro / Not Euro
 SELECT country_name, country_code,
-IF( currency_code = 'EUR', 'Euro', 'Not Euro') AS currency
+IF(currency_code = 'EUR', 'Euro', 'Not Euro') AS currency
 FROM countries 
 ORDER BY country_name;
