31d49ab4c7358f300b453cf3edfeb95d13f55938
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-01-27 16:38:21 +0200

    Create exe.sql

---
+++ MySQL/Table-Relations/Exercises/exe.sql
@@ -0,0 +1,24 @@
+-- Exercise --
+USE geography;
+
+#01. One-To-One Relationship
+
+
+#02. One-To-Many Relationship
+
+
+#03. Many-To-Many Relationship
+
+
+#04. Self-Referencing
+
+
+#05. Online Store Database
+
+
+#06. University Database
+
+
+#09. Peaks in Rila
+
+
