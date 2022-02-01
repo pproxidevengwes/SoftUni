7022734c245fcf448f221b558c9afa2823249a25
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-02-01 12:45:28 +0200

    Update exe_pt2.sql

--- MySQL/Subqueries-and-JOINs/Exercises/geography/exe_pt2.sql
+++ MySQL/Subqueries-and-JOINs/Exercises/geography/exe_pt2.sql
@@ -41,7 +41,18 @@ LIMIT 1)
 ORDER BY c.continent_code, c.currency_code;
 
 #16. Countries without any Mountains
-
+SELECT c.continent_code, c.currency_code,
+COUNT(*) AS 'currency_usage'
+FROM `countries` AS c
+GROUP BY c.continent_code , c.currency_code
+HAVING `currency_usage` > 1 AND `currency_usage` = 
+(SELECT COUNT(*) AS cn
+FROM `countries` AS c2
+WHERE c2.continent_code = c.continent_code
+GROUP BY c2.currency_code
+ORDER BY cn DESC
+LIMIT 1)
+ORDER BY c.continent_code , c.continent_code;
 
 #17. Highest Peak and Longest River by Country
 SELECT c.country_name,
