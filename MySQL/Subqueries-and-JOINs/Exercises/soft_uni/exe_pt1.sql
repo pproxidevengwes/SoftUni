73535bf1023678ed9bd2ca2dad0b54d5f8e1ac28
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-02-01 12:43:43 +0200

    Update exe_pt1.sql

--- MySQL/Subqueries-and-JOINs/Exercises/soft_uni/exe_pt1.sql
+++ MySQL/Subqueries-and-JOINs/Exercises/soft_uni/exe_pt1.sql
@@ -9,12 +9,19 @@ ORDER BY a.address_id
 LIMIT 5;
 
 #02. Addresses with Towns
-
-
+SELECT e.`first_name`, e.`last_name`, t.`name`, a.`address_text`
+FROM `employees` AS e
+JOIN `addresses` AS a USING (`address_id`)
+JOIN `towns` AS t USING (`town_id`)
+ORDER BY e.`first_name` , e.`last_name`
+LIMIT 5;
 
 #03. Sales Employee
-
-
+SELECT e.employee_id, e.first_name, e.last_name, d.name AS `department_name`
+FROM employees AS `e`
+JOIN departments AS `d` ON e.department_id = d.department_id
+WHERE d.`name` = 'Sales'
+ORDER BY e.employee_id DESC;
 
 #04. Employee Departments
 SELECT e.employee_id, e.first_name, e.salary, d.name
@@ -26,10 +33,18 @@ ORDER BY d.department_id DESC
 LIMIT 5;
 
 #05. Employees Without Project
-
+SELECT e.`employee_id`, e.`first_name` FROM employees `e`
+LEFT JOIN employees_projects AS `ep` ON e.employee_id = ep.employee_id
+WHERE ep.`project_id` IS NULL
+ORDER BY e.employee_id DESC LIMIT 3;
 
 #06. Employees Hired After
-
+SELECT e.first_name, e.last_name, e.hire_date, d.name AS 'dept_name'
+FROM employees AS e
+JOIN departments as d
+ON e.department_id = d.department_id
+WHERE e.hire_date > 1999-1-1 AND d.name IN('Sales', 'Finance')
+ORDER BY e.hire_date;
 
 #07. Employees with Project
 SELECT e.employee_id, e.first_name, p.name
@@ -44,7 +59,13 @@ ORDER BY e.first_name, p.name
 LIMIT 5;
 
 #08. Employee 24
-
+SELECT e.`employee_id`, e.`first_name`,
+IF(YEAR(p.`start_date`) >= 2005, NULL, p.`name`) AS `project_name`
+FROM employees AS `e`
+JOIN employees_projects AS `ep` ON ep.employee_id = e.employee_id
+JOIN projects AS `p` ON p.project_id = ep.project_id
+WHERE e.employee_id = 24
+ORDER BY p.`name`;
 
 #09. Employee Manager
 SELECT e.employee_id, e.first_name, e.manager_id, m.first_name
