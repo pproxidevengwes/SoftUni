f4bbb8d3e6ac6032002c3ca39a6d5aba195711bf
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-02-06 19:40:32 +0200

    Update fsd.sql

--- MySQL/exams/09-02-20/fsd.sql
+++ MySQL/exams/09-02-20/fsd.sql
@@ -116,31 +116,81 @@ WHERE `age` < 23 AND `position` = 'A' AND `hire_date` IS NULL
 AND sk.`strength` > 50
 ORDER BY salary;
 
+
+
 #07. Detail info for all teams
-SELECT t.`name` AS 'team_name', `established`, `fan_base`, 
+SELECT t.`name` AS 'team_name',
+t.established, 
+t.fan_base, 
 (SELECT count(id) FROM players WHERE team_id = t.id) AS 'players_count'
 FROM teams AS t
-GROUP BY t.`name`
 ORDER BY players_count DESC, fan_base DESC;
 
 
-
 #08. The fastest player by towns
+SELECT max(sd.speed) AS 'max_speed', t.`name` AS 'town_name' 
+FROM players AS p
+LEFT JOIN skills_data AS sd ON sd.id = p.skills_data_id
+RIGHT JOIN teams AS tm ON tm.id = p.team_id
+RIGHT JOIN stadiums AS s ON s.id = tm.stadium_id
+RIGHT JOIN towns AS t ON t.id = s.town_id
+WHERE tm.name != 'Devify'
+GROUP BY t.`name`
+ORDER BY max_speed DESC, town_name;
 
 
 #09. Total salaries and players by country
-
+SELECT c.`name`,
+count(p.id) AS 'total_count_of_players',
+sum(p.salary) AS 'total_sum_of_salaries'
+FROM countries AS c
+LEFT JOIN towns AS t ON t.country_id = c.id
+LEFT JOIN stadiums AS s ON s.town_id = t.id
+LEFT JOIN teams AS tm ON tm.stadium_id = s.id
+LEFT JOIN players AS p ON p.team_id = tm.id
+GROUP BY c.`name`
+ORDER BY total_count_of_players DESC, c.`name`;
+    
 
 
 #10. Find all players that play on stadium
+DELIMITER $$
+CREATE FUNCTION udf_stadium_players_count(stadium_name VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+RETURN(
+SELECT count(*) FROM players p
+JOIN stadiums AS s ON s.town_id = teams.id
+JOIN teams AS tm ON tm.stadium_id = s.id
+WHERE s.`name` = stadium_name
+);
+END $$
+DELIMITER ;
 
-
+DROP FUNCTION udf_stadium_players_count;
+SELECT udf_stadium_players_count ('Linklinks') as `count`;
 
 #11. Find good playmaker by teams
+DELIMITER $$
+CREATE PROCEDURE udp_find_playmaker(min_dribble_points INT,
+team_name VARCHAR(45))
+BEGIN
+SELECT concat_ws(' ', p.first_name, p.last_name) AS full_name,
+p.age,
+p.salary,
+sd.dribbling,
+sd.speed,
+tm.name AS team_name
+FROM players AS p 
+JOIN skills_data AS sd ON sd.id = p.skills_data_id
+JOIN teams AS tm ON tm.id = p.team_id
+WHERE sd.dribbling > min_dribble_points AND tm.name = team_name
+GROUP BY p.id
+ORDER BY avg(sd.speed) DESC
+LIMIT 1;
+END $$
+DELIMITER ;
 
-
-
-
-
-
-
+DROP PROCEDURE udp_find_playmaker;
+CALL udp_find_playmaker (20, 'Skyble');
