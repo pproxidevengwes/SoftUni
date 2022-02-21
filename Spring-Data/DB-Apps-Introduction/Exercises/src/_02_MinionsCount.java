dd6bb4adf359d5cb4ee449f56f144c06c9ae214f
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-02-21 21:15:57 +0200

    Create _02_MinionsCount.java

---
+++ Spring-Data/DB-Apps-Introduction/Exercises/src/_02_MinionsCount.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+import java.util.Properties;
+
+public class _02_MinionsCount {
+    public static void main(String[] args) throws SQLException {
+        Properties props = new Properties();
+        props.setProperty("user", "root");
+        props.setProperty("password", "amara2113");
+
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/minions_db", props);
+
+        PreparedStatement statement = connection.prepareStatement(
+                "SELECT name, count(distinct mv.minion_id) as minion_count " +
+                        "FROM villains as v " +
+                        "JOIN minions_villains as mv ON mv.villain_id = v.id " +
+                        "GROUP BY mv.villain_id " +
+                        "HAVING minion_count > 15 " +
+                        "ORDER BY minion_count DESC;");
+
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            String villainName = resultSet.getString("name");
+            int minionCount = resultSet.getInt("minion_count");
+            System.out.println(villainName + " " + minionCount);
+        }
+
+        connection.close();
+
+    }
+}
