ff7ccadbfeba7f47197ef5a0f601fa1dec002154
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-02-25 20:22:26 +0200

    Create Main.java

---
+++ Spring-Data/Custom-ORM/miniORM/Main.java
@@ -0,0 +1,26 @@
+import entities.User;
+import orm.EntityManager;
+import orm.MyConnector;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import static orm.MyConnector.createConnection;
+import static orm.MyConnector.getConnection;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException {
+        createConnection("root", "amara2113", "custom_orm");
+        Connection connection = MyConnector.getConnection();
+
+        EntityManager<User> userEntityManager = new EntityManager<>(connection);
+
+        User user = new User("pesho", 25, LocalDate.now());
+
+        userEntityManager.persist(user);
+
+        User foundUser = userEntityManager.findFirst(User.class, "age > 30");
+    }
+}
