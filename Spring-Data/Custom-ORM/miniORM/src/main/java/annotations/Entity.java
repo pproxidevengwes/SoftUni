7010587e039e5613ccbdcc8d53910556b6bcb638
pproxidevengwes <58006459+pproxidevengwes@users.noreply.github.com>
2022-02-28 15:46:20 +0200

    Update and rename Spring-Data/Custom-ORM/miniORM/src/main/java/anotations/Entity.java to Spring-Data/Custom-ORM/miniORM/src/main/java/annotations/Entity.java

--- Spring-Data/Custom-ORM/miniORM/src/main/java/annotations/Entity.java
+++ Spring-Data/Custom-ORM/miniORM/src/main/java/annotations/Entity.java
@@ -1,4 +1,4 @@
-package anotations;
+package annotations;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -6,5 +6,4 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Entity {
     String name();
-
 }
