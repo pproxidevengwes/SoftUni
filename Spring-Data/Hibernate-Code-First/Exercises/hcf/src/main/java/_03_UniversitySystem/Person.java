fce4563e0c9f9cb7dc5e4016e166ab51e2f0baa5
Anna Maria Mitkova <58006459+pproxidevengwes@users.noreply.github.com>
2022-03-07 22:19:03 +0200

    Create Person.java

---
+++ Spring-Data/Hibernate-Code-First/Exercises/hcf/src/main/java/_03_UniversitySystem/Person.java
@@ -0,0 +1,63 @@
+package _03_UniversitySystem;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "people")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private int id;
+
+    @Column(name = "first_name", nullable = false, length = 70)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 70)
+    private String lastName;
+
+    @Column(name = "phone_number", unique = true, length = 40)
+    private String phoneNumber;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
