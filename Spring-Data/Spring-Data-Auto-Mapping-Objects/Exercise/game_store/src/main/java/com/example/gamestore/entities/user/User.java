69a42c5ba9dafc0376a22eb394ecaca540a65fed
Anna Maria Mitkova <aimitkova@gmail.com>
2022-03-25 20:58:24 +0200

    Create User.java

---
+++ Spring-Data/Spring-Data-Auto-Mapping-Objects/Exercise/game_store/src/main/java/com/example/gamestore/entities/user/User.java
@@ -0,0 +1,72 @@
+package com.example.gamestore.entities.user;
+
+
+import com.example.gamestore.entities.BaseEntity;
+import com.example.gamestore.entities.game.Game;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "isAdmin")
+    private boolean isAdmin = false;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Game> games;
+
+    public User() {
+        this.games = new HashSet<>();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Set<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(Set<Game> games) {
+        this.games = games;
+    }
+}
