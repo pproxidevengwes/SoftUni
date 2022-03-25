5bb3626ef0466cb59259543d6ce8a11e0b613b8e
Anna Maria Mitkova <aimitkova@gmail.com>
2022-03-25 20:51:52 +0200

    Create ValidationUtil.java

---
+++ Spring-Data/Spring-Data-Auto-Mapping-Objects/Exercise/game_store/src/main/java/com/example/gamestore/util/ValidationUtil.java
@@ -0,0 +1,11 @@
+package com.example.gamestore.util;
+
+import javax.validation.ConstraintViolation;
+import java.util.Set;
+
+public interface ValidationUtil {
+
+    <E> boolean isValid(E entity);
+
+    <E> Set<ConstraintViolation<E>> violations(E entity);
+}
