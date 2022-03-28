6309fef9ca20229fd43637a3e85b59047c8c213e
Anna Maria Mitkova <aimitkova@gmail.com>
2022-03-28 23:34:30 +0300

    Create  ProductsStatsDto.java

---
+++ Spring-Data/JSON-Processing/jsonex/src/main/productshop/model/dto/ProductsStatsDto.java
@@ -0,0 +1,33 @@
+package com.example.jsonex.productshop.model.dto;
+
+import com.google.gson.annotations.Expose;
+
+import java.math.BigDecimal;
+
+public class ProductsStatsDto {
+
+    @Expose
+    private String name;
+
+    @Expose
+    private BigDecimal price;
+
+    public ProductsStatsDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
