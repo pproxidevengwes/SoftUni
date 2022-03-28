5b1a9e38afb1b1f67abc6b18d3a32e4d0d18a395
Anna Maria Mitkova <aimitkova@gmail.com>
2022-03-28 23:30:04 +0300

    Create ProductService.java

---
+++ Spring-Data/JSON-Processing/jsonex/src/main/productshop/services/ProductService.java
@@ -0,0 +1,14 @@
+package com.example.jsonex.productshop.services;
+
+import com.example.jsonex.productshop.model.dto.ProductNameAndPriceDto;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface ProductService {
+    void seedProducts() throws IOException;
+
+    List<ProductNameAndPriceDto> findAllProductsInRangeOrderByPrice(BigDecimal lower, BigDecimal upper);
+
+}
