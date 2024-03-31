234c1e15c1be40d5e5d9bf9ba8126a9927dc9230
Anna Maria Mitkova <aimitkova@gmail.com>
2024-03-31 13:56:37 +0300

    Add files via upload

---
+++ Spring-Data-2024/Hibernate-Code-First/Lab/CodeFirstHibernate/src/main/java/lab/composition/PlateNumber.java
@@ -0,0 +1,26 @@
+package lab.composition;
+
+import jakarta.persistence.*;
+import lab.inheritance.Car;
+
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "plate_numbers")
+public class PlateNumber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String number;
+
+    @OneToOne(targetEntity = Car.class, mappedBy = "plateNumber")
+    private Car car;
+
+    public PlateNumber() {
+    }
+
+    public PlateNumber(String number) {
+        this.number = number;
+    }
+}
